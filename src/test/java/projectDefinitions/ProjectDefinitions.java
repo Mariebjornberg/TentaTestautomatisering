fee380eee64ebdfdc1ea540db27704319f4d087a
Marie <Marie.bjornberg@hotmail.com>
2021-04-06 16:26:58 +0200

    commmit 5

--- src/test/java/projectDefinitions/ProjectDefinitions.java
+++ src/test/java/projectDefinitions/ProjectDefinitions.java
@@ -27,22 +27,17 @@ public class ProjectDefinitions {
         driver = new ChromeDriver();
         driver.get("https://login.mailchimp.com/signup/");
         driver.manage().window().maximize();
-    sendKeys(driver, By.id("email"), (randomInt + "@unemail.com"));
+    sendKeys(By.id("email"), (randomInt + "@unemail.com"));
 }
 
 
     @Given("I have also entered a {string}")
     public void i_have_also_entered_a_username(String string) throws InterruptedException {
-        if (string.equals("rand")) {
-            sendKeys(driver, By.id("new_username"), (randomInt + "cat"));
-        } else if (string.equals("longName")) {
-
-            sendKeys(driver, By.id("new_username"), (string.repeat(14)));
-
-        } else if (string.equals("existing")) {
-            sendKeys(driver, By.id("new_username"), ("Majsan123"));
-        } else if (string.equals("noMail")) {
-            sendKeys(driver, By.id("new_username"), (randomInt + "fish"));
+        switch (string) {
+            case "rand" -> sendKeys(By.id("new_username"), (randomInt + "cat"));
+            case "longName" -> sendKeys(By.id("new_username"), (string.repeat(14)));
+            case "existing" -> sendKeys(By.id("new_username"), ("Majsan123"));
+            case "noMail" -> sendKeys(By.id("new_username"), (randomInt + "fish"));
         }
 
 
@@ -50,43 +45,48 @@ public class ProjectDefinitions {
 
     @Given("I have also selected a {string}")
     public void i_have_also_selected_a_password(String string) throws InterruptedException {
-        sendKeys(driver, By.id("new_password"), (string));
-        click(driver, By.id("onetrust-accept-btn-handler"));
+        sendKeys(By.id("new_password"), (string));
+        click(By.id("onetrust-accept-btn-handler")); //accept cookies
     }
 
     @When("I press sign up")
     public void i_press_sign_up() throws InterruptedException {
-        click(driver, By.id("create-account"));
+        click(By.id("create-account"));
 
 
     }
 
     @Then("the {string} should be on the screen for {string}")
     public void the_result_should_be_on_the_screen(String string, String string2) throws InterruptedException {
-        if (string2.equals("rand")) {
-            WebElement result = driver.findElement(By.cssSelector("#signup-content > div > div > div > h1"));
-            assertEquals(string, result.getText());
-        } else if (string2.equals("longName")) {
-            WebElement wrong = driver.findElement(By.className("invalid-error"));
-            assertEquals(string, wrong.getText());
-        } else if (string2.equals("existing")) {
-            WebElement wrong = driver.findElement(By.className("invalid-error"));
-            assertEquals(string, wrong.getText());
-        } else if (string2.equals("noMail")) {
-            WebElement wrong = driver.findElement(By.className("invalid-error"));
-            assertEquals(string, wrong.getText());
+        switch (string2) {
+            case "rand" -> {
+                WebElement result = driver.findElement(By.cssSelector("#signup-content > div > div > div > h1"));
+                assertEquals(string, result.getText());
+            }
+            case "longName" -> {
+                WebElement wrong = driver.findElement(By.className("invalid-error"));
+                assertEquals(string, wrong.getText());
+            }
+            case "existing" -> {
+                WebElement wrong = driver.findElement(By.className("invalid-error"));
+                assertEquals(string, wrong.getText());
+            }
+            case "noMail" -> {
+                WebElement wrong = driver.findElement(By.className("invalid-error"));
+                assertEquals(string, wrong.getText());
+            }
         }
 
         driver.quit();
     }
 
-    private void sendKeys(WebDriver driver, By by, String keys) {
+    private void sendKeys(By by, String keys) {
         (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(by));
         driver.findElement(by).sendKeys(keys);
 
     }
 
-    private void click(WebDriver driver, By by) {
+    private void click(By by) {
         (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
         driver.findElement(by).click();
     }
